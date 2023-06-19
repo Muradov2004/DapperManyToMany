fd2a4fdf8dd72d5e1248f6b97ae5722266ecc803
Muradov2004 <ami65143@gmail.com>
2023-06-19 11:53:09 +0400

    Added Query.sql

---
+++ Query.sql
@@ -0,0 +1,14 @@
+﻿CREATE DATABASE ManyToManyDB
+
+USE ManyToManyDB
+
+CREATE TABLE Author(
+[Id] int IDENTITY(1, 1),
+[Name] nvarchar(max) NOT NULL
+)
+
+CREATE TABLE Book(
+[Id] int IDENTITY(1, 1),
+[Name] nvarchar(max) NOT NULL,
+[Price] money NOT NULL
+)
