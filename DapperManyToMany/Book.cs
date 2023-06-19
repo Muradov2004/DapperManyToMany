756a4c83c77cc3f487f8e4eadfc56982de22549a
Muradov2004 <ami65143@gmail.com>
2023-06-19 13:20:42 +0400

    Added Book.cs and Author.cs

---
+++ DapperManyToMany/Book.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DapperManyToMany;
+
+internal class Book
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public List<Author> Authors { get; set; } = new();
+    public override string ToString() => $"{Name} - {Price}";
+}
--- DapperManyToMany/MainWindow.xaml.cs
+++ DapperManyToMany/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +27,29 @@ namespace DapperManyToMany
         public MainWindow()
         {
             InitializeComponent();
+
+            string connectionString;
+
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("AppConfig.json");
+            var config = builder.Build();
+            connectionString = config.GetConnectionString("DefaultConnection")!;
+
+            using (var conn = new SqlConnection(connectionString))
+            {
+                var sql = @"SELECT C.[Id], C.[Name], P.[Id], P.[Name], P.CategoryId
+                            FROM Categories AS C
+                            INNER JOIN Products AS P
+                            ON C.Id = P.CategoryId";
+                var result = conn.Query<Category, Product, Product>(sql,
+                    (category, product) =>
+                    {
+                        product.Category = category;
+                        return product;
+                    });
+                dataGrid.ItemsSource = result;
+            }
         }
     }
 }
