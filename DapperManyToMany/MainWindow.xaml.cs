fef6ccd05b2a286f58603e02e2a3a108fd459b4e
Muradov2004 <ami65143@gmail.com>
2023-06-19 13:26:19 +0400

    Updated

--- DapperManyToMany/MainWindow.xaml.cs
+++ DapperManyToMany/MainWindow.xaml.cs
@@ -1,55 +1,43 @@
 ﻿using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace DapperManyToMany
+namespace DapperManyToMany;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+
+        string connectionString;
+
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory());
+        builder.AddJsonFile("AppConfig.json");
+        var config = builder.Build();
+        connectionString = config.GetConnectionString("DefaultConnection")!;
+
+        using (var conn = new SqlConnection(connectionString))
         {
-            InitializeComponent();
-
-            string connectionString;
-
-            var builder = new ConfigurationBuilder();
-            builder.SetBasePath(Directory.GetCurrentDirectory());
-            builder.AddJsonFile("AppConfig.json");
-            var config = builder.Build();
-            connectionString = config.GetConnectionString("DefaultConnection")!;
-
-            using (var conn = new SqlConnection(connectionString))
-            {
-                var sql = @"SELECT C.[Id], C.[Name], P.[Id], P.[Name], P.CategoryId
-                            FROM Categories AS C
-                            INNER JOIN Products AS P
-                            ON C.Id = P.CategoryId";
-                var result = conn.Query<Category, Product, Product>(sql,
-                    (category, product) =>
-                    {
-                        product.Category = category;
-                        return product;
-                    });
-                dataGrid.ItemsSource = result;
-            }
+            var sql = @"SELECT a.[Id], a.[Name], b.[Name], b.[Price]
+                        FROM AuthorBook AS ab
+                        INNER JOIN Authors AS a
+                        ON a.Id = ab.AuthorId
+                        INNER JOIN Books AS b
+                        ON b.Id = ab.BookId";
+            var result = conn.Query<Author, Book, Author>(sql,
+                (author, book) =>
+                {
+                    author.Books = book;
+                    return product;
+                });
+            dataGrid.ItemsSource = result;
         }
     }
 }
