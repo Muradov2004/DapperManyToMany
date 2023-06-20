a04993a28739925349b183a0574d575a82cf3b52
Muradov2004 <ami65143@gmail.com>
2023-06-20 23:14:44 +0400

    Updated

--- DapperManyToMany/MainWindow.xaml.cs
+++ DapperManyToMany/MainWindow.xaml.cs
@@ -1,10 +1,15 @@
-﻿using Dapper;
+﻿using Azure;
+using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace DapperManyToMany;
 
@@ -27,7 +32,6 @@ public partial class MainWindow : Window
 
         using (var connection = new SqlConnection(connectionString))
         {
-            //var authors = new List<Author>();
 
             var sql = @"SELECT a.[Id], a.[Name], b.[Name], b.[Price]
                         FROM Authors AS a
@@ -35,23 +39,61 @@ public partial class MainWindow : Window
                         ON a.Id = ab.AuthorId
                         INNER JOIN Books AS b
                         ON b.Id = ab.BookId";
-            var result = connection.Query<Author, Book, Author>(sql,
-                    (author, book) =>
-                    {
-                        if (!authors.Exists(c => c.Id == author.Id))
-                        {
-                            author.Books.Add(book);
-                            authors.Add(author);
-                        }
-                        else
-                        {
-                            authors
-                            .FirstOrDefault(a => a.Id == author.Id)!
-                            .Books.Add(book);
-                        }
-                        return author;
-                    });
-            dataGrid.ItemsSource = authors;
+            var authors = connection.Query<Author, Book, Author>(sql,
+                (author, book) =>
+                {
+                    author.Books.Add(book);
+                    return author;
+                },
+            splitOn: "Name");
+
+            var result = authors.GroupBy(a => a.Id).Select(g =>
+            {
+                var groupedBook = g.First();
+                groupedBook.Books = g.Select(b => b.Books.Single()).ToList();
+                return groupedBook;
+            });
+
+            //var mydict = new Dictionary<string, List<string>>()
+            //{
+            //    { "salam1", new() { "salam11","salam12","salam13" } },
+            //    { "salam2", new() { "salam21","salam22","salam23"} },
+            //    { "salam3", new() { "salam31","salam32","salam33"} },
+            //};
+
+            //foreach (var post in result)
+            //{
+            //    Console.Write($"{post.Headline}: ");
+
+            //    foreach (var tag in post.Tags)
+            //    {
+            //        Console.Write($" {tag.TagName} ");
+            //    }
+
+            //    Console.Write(Environment.NewLine);
+            //}
+
+            dataGrid.ItemsSource = result;
+
+            // Create a column for the author name
+            DataGridTextColumn authorColumn = new DataGridTextColumn();
+            authorColumn.Header = "Author";
+            authorColumn.Binding = new Binding("Name");
+            dataGrid.Columns.Add(authorColumn);
+
+            // Iterate over each author to generate book columns dynamically
+            foreach (var author in result)
+            {
+                foreach (Book book in author.Books)
+                {
+                    // Create a column for each book
+                    DataGridTextColumn bookColumn = new DataGridTextColumn();
+                    bookColumn.Header = book.Name;  // Use the book title as the column header
+                    bookColumn.Binding = new Binding($"Books[{author.Books.IndexOf(book)}].Title");
+                    dataGrid.Columns.Add(bookColumn);
+                }
+            }
+
 
         }
     }
